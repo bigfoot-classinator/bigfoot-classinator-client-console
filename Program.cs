cdf36018ffe462335f966caa8d8a7a97c35a00cf
Guy Royse <guy@guyroyse.com>
2018-12-26 13:52:28 -0500

    I am but an empty shell

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace classinator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
